84e80776b6adac62b91e099ea70ea8cb222ede8b
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-05-28 11:53:54 +0600

    add lazy loading

--- src/Components/HomeComponents/Banner/Banner.jsx
+++ src/Components/HomeComponents/Banner/Banner.jsx
@@ -9,6 +9,8 @@ import img2 from '../../../assets/images/blog-2.jpg'
 import img3 from '../../../assets/images/blog-3.jpg'
 import img4 from '../../../assets/images/blog-4.jpg'
 import img5 from '../../../assets/images/banner3.jpg'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 
 const Banner = () => {
@@ -25,19 +27,24 @@ const Banner = () => {
             className='lg:mb-20 mx-5 lg:mx-0'
         >
             <div>
-            <img src={img5} alt="" />
+            <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src={img5}  />
+                {/* <img src={img5} alt="" /> */}
             </div>
             <div className='lg:h-[671px]'>
-                <img src={img1} />
+                {/* <img src={img1} /> */}
+                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src={img1}  />
             </div>
             <div className='lg:h-[671px]'>
-                <img src={img2} />
+                {/* <img src={img2} /> */}
+                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src={img2}  />
             </div>
             <div className='lg:h-[671px]'>
-                <img src={img3} />
+                {/* <img src={img3} /> */}
+                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src={img3}  />
             </div>
             <div className='lg:h-[671px]'>
-                <img src={img4} />
+                {/* <img src={img4} /> */}
+                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src={img4}  />
             </div>
         </Carousel>
     );
--- src/Components/HomeComponents/LatestBlog/LatestBlog.jsx
+++ src/Components/HomeComponents/LatestBlog/LatestBlog.jsx
@@ -2,6 +2,12 @@ import moment from 'moment/moment';
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import the AOS CSS file
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const LatestBlog = () => {
 
@@ -14,11 +20,19 @@ const LatestBlog = () => {
             data-aos-easing="ease-out-cubic"
             data-aos-duration="1500">
             <h2 className='text-center text-3xl font-bold text-accent lg:mb-16'>Latest Blogs</h2>
-            <div className="carousel carousel-center p-4 space-x-4  rounded-box">
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide className='lg:ml-2'>
+                    <div className="card w-96  lg:h-96 bg-base-100 shadow-xl">
                         <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/0YRMjCr/blog-1.jpg" alt="Shoes" className="rounded-xl" />
+                            <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/0YRMjCr/blog-1.jpg"  />
                         </figure>
                         <div className="card-body items-center text-center">
                             <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
@@ -28,92 +42,104 @@ const LatestBlog = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/gSPTjgq/blog-2.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read More</button>
+                </SwiperSlide>
+                <SwiperSlide className='lg:ml-8'>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/gSPTjgq/blog-2.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read More</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/FWrpcL3/blog-3.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read More</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/FWrpcL3/blog-3.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read More</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/BGWHrpw/blog-4.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/BGWHrpw/blog-4.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/vJJ6n9y/blog-5.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/vJJ6n9y/blog-5.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/tbZv4vh/blog-7.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/tbZv4vh/blog-7.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://i.ibb.co/NCmpDkD/blog-8.jpg" alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Read</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="carousel-item">
+                        <div className="card w-96 lg:h-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <LazyLoadImage effect="blur" className='img-fluid w-100 rounded-xl' variant="top" src="https://i.ibb.co/NCmpDkD/blog-8.jpg"  />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{moment().format("MMM Do, YYYY")}</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Read</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
